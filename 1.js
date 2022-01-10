30c55ca360efef5397f8dc61ef9a6ff709a55b25
wangpradit01 <hwangpradisth@gmail.com>
2022-01-10 11:06:52 +0700

    commit

---
+++ 1.js
@@ -0,0 +1,11 @@
+//สูตรคูณ
+//นางสาวศนัญญา หวั่งประดิษฐ์ 6135512036
+
+let a=7;
+    var sum;
+    var i;
+    a=parseInt(a);
+for(i=0;i<13;i++)
+{
+        console.log(sum = a + "X" +i+ "="+(a*i))
+}
