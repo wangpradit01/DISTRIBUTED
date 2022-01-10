3a0c99c849153d45a62c84b2b3ede517b2f2845c
wangpradit01 <hwangpradisth@gmail.com>
2022-01-10 11:06:06 +0700

    https://github.com/wangpradit01/DISTRIBUTED.git

---
+++ 2.js
@@ -0,0 +1,24 @@
+//คิดเกรด
+//นางสาวศนัญญา หวั่งประดิษฐ์ 6135512036
+
+let score = 80;
+
+if (score >= 80) {
+    console.log('Grade A');
+}
+
+else if (score >= 70) {
+    console.log('Grade B');
+}
+
+else if (score >= 60) {
+    console.log('Grade C');
+}
+
+else if (score >= 50) {
+    console.log('Grade D');
+}
+
+else {
+    console.log('Grade E')
+}
